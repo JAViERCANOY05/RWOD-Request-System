aa045b05301722053333f8cc830c485c92464fdb
JavierCanoy <javier@mabolotech.com>
2024-02-16 10:03:39 +0800

    request

--- pages/components/student/Dashboard.tsx
+++ pages/components/student/Dashboard.tsx
@@ -2,16 +2,16 @@ import React from "react";
 
 const Dashboard = () => {
   return (
-    <div className=" flex justify-center my-20 ">
+    <div className=" h-screen flex justify-center my-20 ">
       <div className="">
         <p className=" border-2 p-5 rounded-md font-bold bg-slate-400 text-white ">
           Student Dashboard
         </p>
 
         <div className=" flex gap-10 my-10">
-          <div className="box-content h-32 w-96 p-4  border-4 rounded-md">
-            <p>Pending Request</p>
-            <p className="pt-5">2</p>
+          <div className="box-content h-32 w-96 p-4  border-4 bg-slate-800  rounded-2xl">
+            <p className=" text-white">Pending Request</p>
+            <p className="pt-5 text-white">2</p>
 
             <div className=" flex justify-center mt-5">
               <button
@@ -22,9 +22,9 @@ const Dashboard = () => {
               </button>
             </div>
           </div>
-          <div className="box-content h-32 w-96  p-4 border-4 ... rounded-md">
-            <p>Processing</p>
-            <p className="pt-5">2</p>
+          <div className="box-content h-32 w-96 bg-slate-800  p-4 border-4  rounded-2xl">
+            <p className=" text-white">Processing</p>
+            <p className="pt-5 text-white">2</p>
 
             <div className=" flex justify-center mt-5">
               <button
@@ -37,9 +37,9 @@ const Dashboard = () => {
           </div>
         </div>
         <div className=" flex gap-10 my-10">
-          <div className="box-content h-32 w-96  p-4 border-4 ... rounded-md">
-            <p>Releasing</p>
-            <p className="pt-5">2</p>
+          <div className="box-content h-32 w-96  p-4  border-4 bg-slate-800  rounded-2xl">
+            <p className=" text-white">Releasing</p>
+            <p className="pt-5 text-white">2</p>
 
             <div className=" flex justify-center mt-5">
               <button
@@ -50,9 +50,9 @@ const Dashboard = () => {
               </button>
             </div>
           </div>
-          <div className="box-content h-32 w-96  p-4 border-4 ... rounded-md">
-            <p>Completed</p>
-            <p className="pt-5">2</p>
+          <div className="box-content h-32 w-96  p-4  border-4 bg-slate-800  rounded-2xl">
+            <p className=" text-white">Completed</p>
+            <p className="pt-5 text-white">2</p>
             <div className=" flex justify-center mt-5">
               <button
                 type="button"
