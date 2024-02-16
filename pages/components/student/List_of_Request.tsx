63e7a44fda65d10bbf662e76998247816c3471bc
JavierCanoy <javier@mabolotech.com>
2024-02-16 22:56:53 +0800

    practs-library

--- pages/components/student/List_of_Request.tsx
+++ pages/components/student/List_of_Request.tsx
@@ -12,6 +12,8 @@ import { useForm, SubmitHandler } from "react-hook-form";
 import { notifyError, notifySuccess } from "@/pages/Notifications";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import ListRequest from "@/pages/api/list_request";
+import { useEffect } from "react";
 
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
@@ -50,14 +52,16 @@ interface Column {
     | "Course"
     | "Are you the Owner"
     | "Relationship of Owner"
+    | "Document Name"
     | "No of Copy"
-    | "Total Amount"
+    // | "Total Amount"
+    // | "Date Request"
+    // | "Date Payment"
+    // | "Reference"
+    // | "Proof of Payment"
+    // | "Date Releasing"
+    // | "Processing Officer"
     | "Date Request"
-    | "Date Payment"
-    | "Reference"
-    | "Proof of Payment"
-    | "Date Releasing"
-    | "Processing Officer"
     | "Status"
     | "Action";
 
@@ -70,27 +74,23 @@ interface Column {
 const columns: readonly Column[] = [
   { id: "Control Number", label: "Control Number", minWidth: 170 },
   { id: "User ID No", label: "User ID No", minWidth: 100 },
-  {
-    id: "Name",
-    label: "Name",
-    minWidth: 170,
-    align: "right",
-  },
-  {
-    id: "Course",
-    label: "Course",
-    minWidth: 170,
-    align: "right",
-  },
+
+  // {
+  //   id: "Course",
+  //   label: "Course",
+  //   minWidth: 170,
+  //   align: "right",
+  // },
   {
     id: "Are you the Owner",
     label: "Are you the Owner",
     minWidth: 170,
     align: "right",
   },
+
   {
-    id: "Relationship of Owner",
-    label: "Relationship of Owner",
+    id: "Document Name",
+    label: "Document Name",
     minWidth: 170,
     align: "right",
   },
@@ -101,44 +101,56 @@ const columns: readonly Column[] = [
     align: "right",
   },
   {
-    id: "Total Amount",
-    label: "Total Amount",
+    id: "Relationship of Owner",
+    label: "Relationship of Owner",
     minWidth: 170,
     align: "right",
   },
+  // {
+  //   id: "Total Amount",
+  //   label: "Total Amount",
+  //   minWidth: 170,
+  //   align: "right",
+  // },
+  // {
+  //   id: "Date Request",
+  //   label: "Date Request",
+  //   minWidth: 170,
+  //   align: "right",
+  // },
+  // {
+  //   id: "Date Payment",
+  //   label: "Date Payment",
+  //   minWidth: 170,
+  //   align: "right",
+  // },
+  // {
+  //   id: "Reference",
+  //   label: "Reference",
+  //   minWidth: 170,
+  //   align: "right",
+  // },
+  // {
+  //   id: "Proof of Payment",
+  //   label: "Proof of Payment",
+  //   minWidth: 170,
+  //   align: "right",
+  // },
+  // {
+  //   id: "Date Releasing",
+  //   label: "Date Releasing",
+  //   minWidth: 170,
+  //   align: "right",
+  // },
+  // {
+  //   id: "Processing Officer",
+  //   label: "Processing Officer",
+  //   minWidth: 170,
+  //   align: "right",
+  // },
   {
     id: "Date Request",
     label: "Date Request",
-    minWidth: 170,
-    align: "right",
-  },
-  {
-    id: "Date Payment",
-    label: "Date Payment",
-    minWidth: 170,
-    align: "right",
-  },
-  {
-    id: "Reference",
-    label: "Reference",
-    minWidth: 170,
-    align: "right",
-  },
-  {
-    id: "Proof of Payment",
-    label: "Proof of Payment",
-    minWidth: 170,
-    align: "right",
-  },
-  {
-    id: "Date Releasing",
-    label: "Date Releasing",
-    minWidth: 170,
-    align: "right",
-  },
-  {
-    id: "Processing Officer",
-    label: "Processing Officer",
     minWidth: 170,
     align: "right",
   },
@@ -154,6 +166,7 @@ const columns: readonly Column[] = [
     minWidth: 170,
     align: "right",
   },
+  // documentationType
 ];
 
 interface Data {
@@ -232,6 +245,19 @@ const rows = [
 
 export default function StickyHeadTable() {
   const [page, setPage] = React.useState(0);
+  // const [listRequest, setListRequest] = React.useState({
+  //   controlNumber: "",
+  //   studentId: "",
+  //   emailAddress: "",
+  //   isOwner: "",
+  //   documentationType: "",
+  //   relationshipToOwner: "",
+  //   status: "",
+  //   noOfCopies: "",
+  //   updatedAt: "",
+  // });
+  const [listRequest, setListRequest] = React.useState([]);
+
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
   const {
     reset,
@@ -254,27 +280,26 @@ export default function StickyHeadTable() {
     event.preventDefault();
 
     const dataRequest = {
-      controlNo: data.controlNo,
-      studentID: data.studentID,
+      controlNumber: data.controlNo,
+      studentId: data.studentID,
       emailAddress: data.emailAddress,
-      areYouTheOwner: data.areYouTheOwner,
-      documentName: data.documentName,
-      noCopies: data.noCopies,
-      dateRequest: data.dateRequest,
+      isOwner: data.areYouTheOwner,
+      documentationType: data.documentName,
+      noOfCopies: data.noCopies,
       relationshipToOwnwer: data.relationshipToOwnwer,
     };
+
     const token = localStorage.getItem("token");
-
-    console.log(dataRequest, "data form request ! ", "token", token);
-
+    console.log(dataRequest, "data form request ! ");
     try {
-      const response = await RequestForm.request(dataRequest, token);
-      console.log(response, " hhehe");
-      if (response.message === "Add succesfully") {
-        notifySuccess(response.meesage);
+      const data = await RequestForm.request(dataRequest, token);
+      if (data.status) {
+        notifySuccess("Add succesfully");
+        getListOfRequest();
         reset();
         setOpen(false);
       } else {
+        notifyError("Something went wrong ! ");
         console.log("error ");
       }
     } catch (error) {
@@ -293,22 +318,42 @@ export default function StickyHeadTable() {
     setRowsPerPage(+event.target.value);
     setPage(0);
   };
+  const getListOfRequest = async () => {
+    const token = localStorage.getItem("token");
+    try {
+      const response = await ListRequest.list(token);
+      console.log(response, "99999999");
+      if (response) {
+        setListRequest(response);
+      } else {
+        console.log("Invalid response data:", response);
+      }
+
+      console.log(response);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    getListOfRequest();
+  }, []);
 
   return (
-    <div>
+    <div className=" h-screen">
       <div className="my-10">
         <p className=" font-bold border-2 rounded-md p-5 bg-slate-400 text-white">
           Request Information
         </p>
       </div>
       <div>
-        <button
-          onClick={handleOpen}
-          type="button"
-          className="mx-20 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
-        >
-          Add Request
-        </button>
+        <div className=" flex justify-end mx-40">
+          <button
+            onClick={handleOpen}
+            type="button"
+            className="mx-20 focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+          >
+            Add Request
+          </button>
+        </div>
         <div className=" flex justify-center">
           <Box
             sx={{
@@ -316,7 +361,7 @@ export default function StickyHeadTable() {
               flexWrap: "wrap",
               "& > :not(style)": {
                 m: 1,
-                width: 1500,
+                width: 1700,
                 height: "100%",
               },
             }}
@@ -339,80 +384,86 @@ export default function StickyHeadTable() {
                     </TableRow>
                   </TableHead>
                   <TableBody>
-                    {rows
-                      .slice(
-                        page * rowsPerPage,
-                        page * rowsPerPage + rowsPerPage
-                      )
-                      .map((row) => {
-                        return (
-                          <TableRow
-                            hover
-                            role="checkbox"
-                            tabIndex={-1}
-                            key={row.Are_you_the_Owner}
-                          >
-                            <TableCell>{row.Control_Number}</TableCell>
-                            <TableCell>{row.Course}</TableCell>
-                            <TableCell align="right">
-                              {row.Date_Payment}
-                            </TableCell>
-                            <TableCell align="right">
-                              {row.Date_Releasing}
-                            </TableCell>
-                            <TableCell align="right">
-                              {row.Date_Request}
-                            </TableCell>
-                            <TableCell align="right">
-                              {row.Date_Request}
-                            </TableCell>
-                            <TableCell align="right">
-                              {row.Date_Request}
-                            </TableCell>
-                            <TableCell align="right">
-                              {row.Date_Request}
-                            </TableCell>
-                            <TableCell align="right">
-                              {row.Date_Request}
-                            </TableCell>
-                            <TableCell align="right">
-                              {row.Date_Request}
-                            </TableCell>
-                            <TableCell align="right">
-                              {row.Date_Request}
-                            </TableCell>
-                            <TableCell align="right">
-                              {row.Date_Request}
-                            </TableCell>
-                            <TableCell align="right">
-                              {row.Date_Request}
-                            </TableCell>
-                            <TableCell align="right">
-                              {row.Date_Request}
-                            </TableCell>
-                            <TableCell align="right">
-                              {row.Date_Request}
-                            </TableCell>
-                            <TableCell align="right">
-                              <div className=" flex gap-2">
-                                <button
-                                  type="button"
-                                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-                                >
-                                  Update
-                                </button>
+                    {listRequest.length === 0 ? (
+                      <TableRow>
+                        <TableCell colSpan={9} align="center">
+                          <div role="status" className=" flex justify-center">
+                            <svg
+                              aria-hidden="true"
+                              className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                              viewBox="0 0 100 101"
+                              fill="none"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <path
+                                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                fill="currentColor"
+                              />
+                              <path
+                                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                fill="currentFill"
+                              />
+                            </svg>
+                          </div>
+                        </TableCell>
+                      </TableRow>
+                    ) : (
+                      listRequest
+                        .slice(
+                          page * rowsPerPage,
+                          page * rowsPerPage + rowsPerPage
+                        )
+                        .map((list: any, index = 0) => {
+                          return (
+                            <TableRow
+                              hover
+                              role="checkbox"
+                              tabIndex={-1}
+                              key={index++}
+                            >
+                              <TableCell>{list.controlNumber}</TableCell>
+                              <TableCell>{list.ownerId}</TableCell>
+                              <TableCell align="right">
+                                {list.isOwner}
+                              </TableCell>
+                              <TableCell align="right">
+                                {list.documentationType}
+                              </TableCell>
+                              <TableCell align="right">
+                                {list.noOfCopies}
+                              </TableCell>
 
-                                <button
-                                  type="button"
-                                  className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
-                                >
-                                  Delete
-                                </button>
-                              </div>
-                            </TableCell>
-                          </TableRow>
-                        );
-                      })}
+                              <TableCell align="right">
+                                <p>wala pa </p>
+                              </TableCell>
+                              <TableCell align="right">
+                                {new Date(list.createdAt).toLocaleString()}
+                              </TableCell>
+                              <TableCell align="right">
+                                <span> {list.status}</span>
+                              </TableCell>
+
+                              <TableCell align="right">
+                                <div className=" flex gap-2">
+                                  <button
+                                    type="button"
+                                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                                  >
+                                    Update
+                                  </button>
+
+                                  <button
+                                    type="button"
+                                    className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                                  >
+                                    Delete
+                                  </button>
+                                </div>
+                              </TableCell>
+                            </TableRow>
+                          );
+                        })
+                    )}
                   </TableBody>
                 </Table>
               </TableContainer>
@@ -526,7 +577,7 @@ export default function StickyHeadTable() {
                   {errors.noCopies && <span>This field is required</span>}
                 </div>
               </div>
-              <div>
+              {/* <div>
                 <p className=" mx-2 mt-3 mb-1 ">Date Request</p>
                 <input
                   className=" mx-2 rounded-md py-3 px-10"
@@ -535,7 +586,7 @@ export default function StickyHeadTable() {
                 <div className=" mx-2 text-yellow-500">
                   {errors.dateRequest && <span>This field is required</span>}
                 </div>
-              </div>
+              </div> */}
             </div>
             <div className=" flex justify-center my-10 mx-2 gap-5">
               {/* <Link href="/"> */}
